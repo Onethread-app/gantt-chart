1b6e70709ab7f9731e08b17aa54c79ea6dca9c39
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2022-06-06 14:10:32 +0600

    node modules removed, onthread-gantt-chart package integration

--- node_modules/gantt-task-react/dist/components/other/horizontal-scroll.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { SyntheticEvent } from "react";
-export declare const HorizontalScroll: React.FC<{
-    scroll: number;
-    svgWidth: number;
-    taskListWidth: number;
-    rtl: boolean;
-    onScroll: (event: SyntheticEvent<HTMLDivElement>) => void;
-}>;
--- node_modules/gantt-task-react/dist/components/other/vertical-scroll.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { SyntheticEvent } from "react";
-export declare const VerticalScroll: React.FC<{
-    scroll: number;
-    ganttHeight: number;
-    ganttFullHeight: number;
-    headerHeight: number;
-    rtl: boolean;
-    onScroll: (event: SyntheticEvent<HTMLDivElement>) => void;
-}>;
